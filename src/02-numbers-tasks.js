7303aca7d115d09904f2d0de610ebb0fbbb224a4
Agnia <agnia2000@inbox.ru>
2020-05-11 00:43:26 +0300

    feat: finish 02-numbers

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -69,8 +69,8 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints( /* x1, y1, x2, y2 */ ) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.hypot(x2 - x1, y2 - y1);
 }
 
 /**
@@ -85,8 +85,8 @@ function getDistanceBetweenPoints( /* x1, y1, x2, y2 */ ) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot( /* a, b */ ) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 
@@ -108,8 +108,8 @@ function getLinearEquationRoot( /* a, b */ ) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors( /* x1, y1, x2, y2 */ ) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  return Math.acos(((x1 * x2) + (y1 * y2)) / (Math.hypot(x1, y1) * Math.hypot(x2, y2)));
 }
 
 /**
@@ -124,8 +124,8 @@ function getAngleBetweenVectors( /* x1, y1, x2, y2 */ ) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit( /* value */ ) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 
@@ -140,8 +140,8 @@ function getLastDigit( /* value */ ) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString( /* value */ ) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -157,8 +157,8 @@ function parseNumberFromString( /* value */ ) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal( /* a, b, c */ ) {
-  throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -178,8 +178,9 @@ function getParallelipidedDiagonal( /* a, b, c */ ) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen( /* num, pow */ ) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const tenPow = 10 ** pow;
+  return Math.round(num / tenPow) * tenPow;
 }
 
 /**
@@ -199,8 +200,13 @@ function roundToPowerOfTen( /* num, pow */ ) {
  *   16 => false
  *   17 => true
  */
-function isPrime( /* n */ ) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return n > 1;
 }
 
 /**
@@ -218,8 +224,9 @@ function isPrime( /* n */ ) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber( /* value, def */ ) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const x = parseInt(value, 10);
+  return Number.isNaN(x) ? def : x;
 }
 
 module.exports = {
